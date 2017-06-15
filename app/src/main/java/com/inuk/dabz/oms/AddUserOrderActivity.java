823936aa7d8112cf4041d123f6677d25fc15133d
abhosale <dabzinuk@gmail.com>
2017-06-15 09:01:26 +0100

    added cancel

--- app/src/main/java/com/inuk/dabz/oms/AddUserOrderActivity.java
+++ app/src/main/java/com/inuk/dabz/oms/AddUserOrderActivity.java
@@ -1,13 +1,32 @@
 package com.inuk.dabz.oms;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AddUserOrderActivity extends AppCompatActivity {
-
+    private Button btnUserCancelActivity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_user_order);
+
+
+        btnUserCancelActivity = (Button) findViewById(R.id.cancelOrderbtn);
+
+        btnUserCancelActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                launchActivity();
+            }
+        });
+    }
+    private void launchActivity() {
+
+        Intent intent = new Intent(this, ListUserOrderActivity.class);
+        startActivity(intent);
     }
 }
